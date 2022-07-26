d70178647e164940ad59eef106922974f06d4c2d
Pedro Calderon <pedrojosecalderon0026@gmail.com>
2022-07-25 19:59:29 -0500

    model_user

---
+++ main.py
@@ -0,0 +1,47 @@
+# Python
+from unittest.mock import Base
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# FastAPI
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8
+    )
+    
+class User(UserBase):
+    firs_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birt_date: Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    pass
+
+@app.get(path = '/')
+def home():
+    return {'Twitter API': 'Working!'}
